fbabc0c3878faddba6b73226af63ee1725caa8a3
IsmaelOlivarez <ismael.olivarez05@gmail.com>
2024-04-11 17:17:41 -0400

    Initial Commit

---
+++ Foodies/FoodiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodiesApp.swift
+//  Foodies
+//
+//  Created by Ismael Olivarez on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
