8cbe5a452576a450a578aed22020ed7af90314ff
Samarium99 <sarah.mo@csedge.org>
2024-05-23 17:54:31 -0400

    (ehinowen) added actual info to the screens i made

--- Foodies/ContentView.swift
+++ Foodies/ContentView.swift
@@ -13,22 +13,28 @@ struct ContentView: View
     @State private var name : String = "";
     @State private var weight : Int? = nil;
     var body: some View {
-        Spacer().frame(height:60)
-        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-        {
-            Text("Hi!").font(.system(size: 128)).bold()
-            Text("Please enter your name and weight.").font(.system(size: 24)).multilineTextAlignment(.center)
+        NavigationStack {
+            Spacer().frame(height:60)
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            {
+                Text("Hi!").font(.system(size: 128)).bold()
+                Text("Please enter your name and weight.").font(.system(size: 24)).multilineTextAlignment(.center)
+                Spacer()
+                TextField("Your Name", text: $name).font(.system(size:inputSize)).padding()
+                Spacer()
+                TextField("Your Weight", value: $weight, format: .number ).font(.system(size:inputSize)).padding()
+            }.frame(minHeight: 0, maxHeight: 400).padding()
+            Button {
+                
+            } label: {
+                NavigationLink {
+                    NutritionGoals()
+                } label: {
+                    Text("Continue").font(.system(size:64)).padding(24)
+                }
+            }
             Spacer()
-            TextField("Your Name", text: $name).font(.system(size:inputSize)).padding()
-            Spacer()
-            TextField("Your Weight", value: $weight, format: .number ).font(.system(size:inputSize)).padding()
-        }.frame(minHeight: 0, maxHeight: 400).padding()
-        Button {
-            
-        } label: {
-            Text("Continue").font(.system(size:64)).padding(24)
         }
-        Spacer()
     }
 }
 
--- Foodies/NutritionGoals.swift
+++ Foodies/NutritionGoals.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct NutritionGoals: View
+{
+    private let goldenRatio : CGFloat = 1.618
+    private let inputSize : CGFloat = 32;
+    @State private var calorieGoal: Int? = nil;
+    @State private var fatGoal: Int? = nil;
+    @State private var carbGoal: Int? = nil;
+    @State private var proteinGoal: Int? = nil;
+    @State private var sodiumGoal: Int? = nil;
+    var body : some View {
+        NavigationStack {
+            VStack() {
+                Spacer().frame(height: 60)
+                TextField("Calories", value: $calorieGoal, format: .number).font(.system(size: inputSize)).border(.black).frame(width: UIScreen.screenWidth / goldenRatio)
+                TextField("Fat", value: $fatGoal, format: .number).font(.system(size: inputSize)).border(.black).frame(width: UIScreen.screenWidth / goldenRatio)
+                TextField("Carbohydrates", value: $carbGoal, format: .number).font(.system(size: inputSize)).border(.black).frame(width: UIScreen.screenWidth / goldenRatio)
+                TextField("Protein", value: $proteinGoal, format: .number).font(.system(size: inputSize)).border(.black).frame(width: UIScreen.screenWidth / goldenRatio)
+                TextField("Sodium", value: $sodiumGoal, format: .number).font(.system(size: inputSize)).border(.black).frame(width: UIScreen.screenWidth / goldenRatio)
+                Spacer()
+            }.navigationTitle("Nutrition Goals")
+        }
+    }
+}
+
+#Preview {
+    NutritionGoals()
+}
